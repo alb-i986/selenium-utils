91de123c600a93e227db4103d71f108001eeb11f
Alberto Scotto <scotto.alberto.86@gmail.com>
2020-06-06 20:50:42 +0200

    PageObject: give subclasses quick access to the underlying driver

--- src/main/java/me/alb_i986/selenium/utils/PageObject.java
+++ src/main/java/me/alb_i986/selenium/utils/PageObject.java
@@ -1,10 +1,14 @@
 package me.alb_i986.selenium.utils;
 
+import org.openqa.selenium.WebDriver;
+
 public abstract class PageObject {
 
     protected final SeleniumHelper seleniumHelper;
+    protected final WebDriver driver;
 
     public PageObject(SeleniumHelper seleniumHelper) {
         this.seleniumHelper = seleniumHelper;
+        this.driver = seleniumHelper.getDriver();
     }
 }
